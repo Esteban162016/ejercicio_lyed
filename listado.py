983a041b54d3d7f44f718a562684d4f074b6845e
Esteban Ugarte <esteban.ugarte@alumnos.ispi4038.edu.ar>
2025-07-01 17:42:39 -0300

    Se agrega a git ignore listado.py

--- listado.py
+++ listado.py
@@ -3,10 +3,23 @@ poblacion.readline()  # leo la linea 1 para ignorarla
 lista_poblacion = poblacion.readlines()  # leo el resto del archivo("")
 poblacion.close() # cierro el archivo
 total_habitantes = 0  # inicializo el total de habitantes
+provincias = set()
 
 for linea in lista_poblacion: # iterando cada elemento de la lista
     datos = linea.split(",")  # separar la línea por comas
     cantidad_habitantes = int(datos[6]) # obtener la cantidad de habitantes de cada renglón
     total_habitantes = total_habitantes + cantidad_habitantes  # sumar la cantidad de habitantes al total
+    
+    #provincia = datos[1].strip()
+    #provincias.add(provincia) 
+    
+    provincias.add(datos[1].strip())# es lo mismo que hacer lo anterior
 
-print(f"Total de habitantes: {total_habitantes:,}")  # imprime el total de habitantes
+provincias = sorted(provincias)   
+ 
+cantidad = 0
+for p in provincias:
+    print(p)
+    cantidad += 1
+print('cantidad de provincias: ', cantidad)
+print(f"Total de habitantes: {total_habitantes:,}")
